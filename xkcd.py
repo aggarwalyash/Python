a8a4e57069f7e3132131d0ebb213283de7ba286f
Yash Aggarwal <yashaggarwal212@gmail.com>
2017-02-17 23:04:48 +0530

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -6,12 +6,12 @@ end = int(input())
 os.makedirs('xkcd',exist_ok = True)
 for i in range(start,end+1):
     url = "http://xkcd.com/"
-	url += str(i) + "/"
-    print("Downloading page %s" % url)
-	r = requests.get(url)
-	soup = bs4.BeautifulSoup(r.text)
-	comic = soup.select("#comic img")
-	comic_url = 'http:' + comic[0].get('src')
-	print("Downloading image %s" %s(comic_url))
+    url += str(i) + "/"
+    print("Downloading page %s" % url) 
+    r = requests.get(url)
+    soup = bs4.BeautifulSoup(r.text)
+    comic = soup.select("#comic img")
+    comic_url = 'http:' + comic[0].get('src')
+    print("Downloading image %s" %s(comic_url))
     urllib.request.urlretrieve(comic_url)
-	print("Downnloaded")
+    print("Downnloaded")
