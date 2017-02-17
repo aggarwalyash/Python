9fee5b11b299885eacc9c8b5cf2c8004b3530389
Yash Aggarwal <yashaggarwal212@gmail.com>
2017-02-17 23:08:21 +0530

    Update random.py

--- random.py
+++ random.py
@@ -4,18 +4,19 @@ guess = 100
 guess_done = 0
 while guess>0:
     n = int(input("Guess the number:"))
-	if(num == n):
-	    print("Congrats %d" %(score))
-		break
+    if(num == n):
+	print("Congrats %d" %(score))
+	break
     guess_done += 1
+    guess -= 1 
     if (n>=1 and n<25):
-	    print("Your guess is too low")
+        print("Your guess is too low")
     elif (n>=25 and n<50):
-	    print("Your guess is low")
-	elif (n>=50 and n<75):
-	    print("Your guess is high")
-	elif (n>=75 and n<100):
-	    print("Your guess is too high")
+	print("Your guess is low")
+    elif (n>=50 and n<75):
+	print("Your guess is high")
+    elif (n>=75 and n<100):
+	print("Your guess is too high")
 else:
     print("No more atrempts left. Have a nice day!!!!!!!!")
 score = 100-guess_done
